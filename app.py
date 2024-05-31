a1aa32085f837729caf4ff375909cbd4e14d2c04
Jvpavankumar <90099464+Jvpavankumar@users.noreply.github.com>
2024-05-31 20:37:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True,nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     user_type = db.Column(db.String(20), nullable=False)
