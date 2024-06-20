35f04d7032bfc6086d8099c3f219344f61f652c1
Jvpavankumar <90099464+Jvpavankumar@users.noreply.github.com>
2024-06-20 18:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6102,9 +6102,9 @@ def post_job():
                 if is_valid_email(recruiter.email):
                     # error_msg = post_job_send_notification(recruiter.email, recruiter.username, job_data)
                     pass
-                    if error_msg:
+                    # if error_msg:
                         # return jsonify({'status': 'error', 'message': error_msg}), 500
-                        pass
+                        # pass
                 else:
                     invalid_emails.append(recruiter.email)
 
