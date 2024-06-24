88a487d1b990a366fc1c9081b0476b49551ac523
Jvpavankumar <90099464+Jvpavankumar@users.noreply.github.com>
2024-06-24 22:16:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10325,7 +10325,7 @@ def get_time_to_close(query, from_date, to_date):
     ).with_entities(
         Candidate.recruiter,
         func.avg(
-            func.EXTRACT(
+            func.date_part(
                 'day',
                 Candidate.last_working_date - Candidate.date_created
             )
